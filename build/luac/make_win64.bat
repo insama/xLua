6a4e2c764aee189a738af4570b1e0a4febe8937f
Yue <82847014@qq.com>
2020-10-30 19:45:27 +0800

    修复luac编译后拷贝路径不正确的问题

--- build/luac/make_win64.bat
+++ build/luac/make_win64.bat
@@ -4,5 +4,5 @@ IF %ERRORLEVEL% NEQ 0 cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 15 201
 popd
 cmake --build build64 --config Release
 md luac_lua53\win
-copy /Y build64\luac.exe luac_lua53\win\luac.exe
+copy /Y build64\Release\luac.exe luac_lua53\win\luac.exe
 pause
--- build/luac/make_win64_lua54.bat
+++ build/luac/make_win64_lua54.bat
@@ -4,5 +4,5 @@ IF %ERRORLEVEL% NEQ 0 cmake -DLUAC_COMPATIBLE_FORMAT=ON -DLUA_VERSION=5.4.1 -G "
 popd
 cmake --build build64_54 --config Release
 md luac_lua54\win
-copy /Y build64_54\luac.exe luac_lua54\win\luac.exe
+copy /Y build64_54\Release\luac.exe luac_lua54\win\luac.exe
 pause
