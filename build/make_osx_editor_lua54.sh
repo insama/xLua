be02ce5fb5c90012259eba6520138b6616187543
岳溁松 <82847014@qq.com>
2020-10-30 18:24:22 +0800

    修复编译mac_editor库使用的lua版本不对的问题

--- build/make_osx_editor_lua54.sh
+++ build/make_osx_editor_lua54.sh
@@ -1,5 +1,5 @@
 mkdir -p build_osx_editor_54 && cd build_osx_editor_54
-cmake -DLUAC_COMPATIBLE_FORMAT=ON -DDISABLE_SOURCECODE=OFF -GXcode ../
+cmake -DLUAC_COMPATIBLE_FORMAT=ON -DDISABLE_SOURCECODE=OFF -DLUA_VERSION=5.4.1 -GXcode ../
 cd ..
 cmake --build build_osx_editor_54 --config Release
 mkdir -p plugin_lua54/Plugins/Editor/OSX/xlua.bundle/Contents/MacOS/
