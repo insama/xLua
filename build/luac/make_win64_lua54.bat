0921cae35a61a30879a6d1e993f770310e18f96c
岳溁松 <yueyingsong@rscube.cn>
2022-05-06 17:12:13 +0800

    Revert "尝试修复windows报错问题"
    This reverts commit b0cff61b48351f5e887a26076523d9fbbfb5c126.

--- build/luac/make_win64_lua54.bat
+++ build/luac/make_win64_lua54.bat
@@ -1,6 +1,6 @@
 mkdir build64_54 & pushd build64_54
-cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 16 2019" -A "Win64"
-IF %ERRORLEVEL% NEQ 0 cmake -DLUAC_COMPATIBLE_FORMAT=ON -DLUA_VERSION=5.4.1 -G "Visual Studio 16 2019" -A "Win64"
+cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 16 2019" -A "Win64" ..
+IF %ERRORLEVEL% NEQ 0 cmake -DLUAC_COMPATIBLE_FORMAT=ON -DLUA_VERSION=5.4.1 -G "Visual Studio 16 2019" -A "Win64" ..
 popd
 cmake --build build64_54 --config Release
 md luac_lua54\win
--- build/make_win_lua54.bat
+++ build/make_win_lua54.bat
@@ -1,5 +1,5 @@
 mkdir build64_54 & pushd build64_54
-cmake -DLUAC_COMPATIBLE_FORMAT=ON -DLUA_VERSION=5.4.1 -G "Visual Studio 16 2019" -A "Win64"
+cmake -DLUAC_COMPATIBLE_FORMAT=ON -DLUA_VERSION=5.4.1 -G "Visual Studio 16 2019" -A "Win64" ..
 popd
 cmake --build build64_54 --config Release
 md plugin_lua54\Plugins\Windows\x86_64
