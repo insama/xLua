fdf7a7fb22418922da404415f1fd829aca975f14
岳溁松 <yueyingsong@rscube.cn>
2022-05-06 16:01:06 +0800

    修复Windows编译失败的问题

--- build/make_uwp.bat
+++ build/make_uwp.bat
@@ -6,14 +6,14 @@ md plugin_lua53\Plugins\WSA\x86
 copy /Y build_uwp\Release\xlua.dll plugin_lua53\Plugins\WSA\x86\xlua.dll
 
 mkdir build_uwp64 & pushd build_uwp64
-cmake -G "Visual Studio 16 2019 -A Win64" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 ..
+cmake -G "Visual Studio 16 2019" -A "Win64" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 ..
 popd
 cmake --build build_uwp64 --config Release
 md plugin_lua53\Plugins\WSA\x64
 copy /Y build_uwp64\Release\xlua.dll plugin_lua53\Plugins\WSA\x64\xlua.dll
 
 mkdir build_uwp_arm & pushd build_uwp_arm
-cmake -G "Visual Studio 16 2019 -A ARM" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 ..
+cmake -G "Visual Studio 16 2019 "-A" ARM" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 ..
 popd
 cmake --build build_uwp_arm --config Release
 md plugin_lua53\Plugins\WSA\ARM
